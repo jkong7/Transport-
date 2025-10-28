308e4ddfb9fa22f902947d147eebfe95f5bdb1f2
Jonathan Kong <jonathankong677@gmail.com>
2025-10-28 17:19:03 -0500

    Initial draft of receiver receive finished

--- wildcat_receiver.py
+++ wildcat_receiver.py
@@ -1,5 +1,7 @@
 import common
 import threading
+import struct
+import zlib
 
 class wildcat_receiver(threading.Thread):
     def __init__(self, allowed_loss, window_size, my_tunnel, my_logger):
@@ -9,14 +11,59 @@ class wildcat_receiver(threading.Thread):
         self.my_tunnel = my_tunnel
         self.my_logger = my_logger
         self.die = False
-        self.buffer = [] * window_size # buffer to hold out-of-order packets
-        self.next_expected_seq_num = 0
+
+        self.receiver_window_start = 0
+        self.receiver_window = {} # seq_num: payload
+        self.wrap_around = 65536
+    
+    def parse_sender_msg(self, packet_byte_array):
+        seq_num = struct.unpack("!H", packet_byte_array[:2])[0]
+        checksum_received = struct.unpack("!H", packet_byte_array[-2:])[0]
+        checksum_recomputed = zlib.crc32(packet_byte_array[:-2]) & 0xffff
+        if checksum_received != checksum_recomputed:
+            return None, None
+        payload = packet_byte_array[2:-2]
+        return seq_num, payload
+
+    def build_ack(self, window_start, bitmap):
+        first_two = struct.pack("!H", window_start)
+        bitmap_bytes = bytearray()
+        for i in range(0, len(bitmap), 8):
+            byte = 0
+            for j in range(8):
+                if i + j < len(bitmap):
+                    byte |= (bitmap[i + j] << j)
+            bitmap_bytes.append(byte)
+        checksum = zlib.crc32(first_two + bitmap_bytes) & 0xffff
+        last_two = struct.pack("!H", checksum)
+        return first_two + bitmap_bytes + last_two
 
     def receive(self, packet_byte_array):
-        # TODO: your implementation comes here
-        seq_num = int.from_bytes(packet_byte_array[0:2], byteorder='big')
+        seq_num, payload = self.parse_sender_msg(packet_byte_array)
+
+        # Record packet in receiver window (if not corrupted)
+        # We always send ack here, even for corrupted packets and for out-of-window packets
+        if seq_num is not None:
+            self.receiver_window[seq_num] = payload
+        bitmap=[]
+
+        # Go through window range and build bitmap
+        for offset in range(self.window_size):
+            seq_num = (self.receiver_window_start + offset) % self.wrap_around
+            bitmap.append(1 if seq_num in self.receiver_window else 0)
         
-        pass
+        # Build and send ack, also log payload 
+        ack = self.build_ack(self.receiver_window_start, bitmap)
+        self.my_tunnel.magic_send(ack)
+
+        # Update window start and commit in-order packets to logger (out of order packet buffered in window
+        # but won't be committed to log until in-order, i.e. no gaps)
+        if seq_num == self.receiver_window_start:
+            while self.receiver_window_start in self.receiver_window:
+                self.my_logger.commit(self.receiver_window[self.receiver_window_start])
+                self.receiver_window_start = (self.receiver_window_start + 1) % self.wrap_around
+
+        
 
     def run(self):
         while not self.die:
--- wildcat_sender.py
+++ wildcat_sender.py
@@ -14,10 +14,10 @@ class wildcat_sender(threading.Thread):
         self.my_logger = my_logger
         self.die = False
 
-        self.window = [] # list of (seq_num, packet, timestamp), holds packets in flight
+        self.sender_window = [] # list of (seq_num, packet, timestamp), holds packets in flight
         self.packet_queue = queue.Queue() # queue of packets to be sent
         self.next_seq_num = 0 # seq num of next packet to be sent
-        self.window_start = 0 # seq num of first packet in sender window
+        self.sender_window_start = 0 # seq num of first packet in sender window
         self.timeout_interval = 0.5 # allow 0.5s for timeout
         self.wrap_around = 65536 # seq num wrap around value
         self.lock = threading.Lock() # lock for global variables
@@ -42,7 +42,7 @@ class wildcat_sender(threading.Thread):
             for i in range(8):
                 if len(bits) < self.window_size:
                     bits.append((bytes >> i) & 1)
-        return receiver_window_start, bits, checksum_received
+        return receiver_window_start, bits
     
     def new_packet(self, packet_byte_array):
         with self.lock: # Just acquire lock and enqueue packets
@@ -50,28 +50,28 @@ class wildcat_sender(threading.Thread):
         
 
     def receive(self, packet_byte_array):
-        receiver_window_start, bits, _ = self.parse_ack(packet_byte_array)
+        receiver_window_start, bits = self.parse_ack(packet_byte_array)
         if receiver_window_start is None: 
             return # corrupted ACK, no handling
 
         # Remove all packets acked by receiver (check bitmap)
         with self.lock: 
             to_remove = []
-            for packet in sorted(self.window, key = lambda x: x[0]):
+            for packet in sorted(self.sender_window, key = lambda x: x[0]):
                 seq_num, _, _ = packet
                 relative_seq_num = (seq_num - receiver_window_start) % self.wrap_around
                 if relative_seq_num < self.window_size:
                     if bits[relative_seq_num] == 1:
                         to_remove.append(packet)
             for packet in to_remove:
-                self.window.remove(packet)
+                self.sender_window.remove(packet)
             
             # Window start should now be lowest seq num in window
-            if self.window:
-                self.window_start = self.window[0][0]
+            if self.sender_window:
+                self.sender_window_start = self.sender_window[0][0]
             # Or if everything acked, window start aligns to next seq num
             else: 
-                self.window_start = self.next_seq_num 
+                self.sender_window_start = self.next_seq_num 
     
     
     def run(self):
@@ -80,24 +80,24 @@ class wildcat_sender(threading.Thread):
             while True: 
                 with self.lock: # Acquire lock as global variables are accessed
                     # If the window isn't full and there are packets to send, we continuously send
-                    if len(self.window) < self.window_size and not self.packet_queue.empty():
+                    if len(self.sender_window) < self.window_size and not self.packet_queue.empty():
                         # Pull from queue, build msg, send it, and add to window for bookkeeping
                         packet = self.packet_queue.get()
                         msg = self.build_msg(packet, self.next_seq_num)
                         self.my_tunnel.magic_send(msg)
-                        self.window.append( (self.next_seq_num, packet, cur_time) )
+                        self.sender_window.append( (self.next_seq_num, packet, cur_time) )
                         self.next_seq_num = (self.next_seq_num + 1) % self.wrap_around
                     else: 
                         break # Otherwise, break out of sending loop and move on to checking timeouts
 
             with self.lock:
-                for i in range(len(self.window)):
+                for i in range(len(self.sender_window)):
                     # Any timeouts, resend and update timestamp in window
-                    seq_num, packet, timestamp = self.window[i]
+                    seq_num, packet, timestamp = self.sender_window[i]
                     if cur_time - timestamp > self.timeout_interval:
                         msg = self.build_msg(packet, seq_num)
                         self.my_tunnel.magic_send(msg)
-                        self.window[i] = (seq_num, packet, cur_time) 
+                        self.sender_window[i] = (seq_num, packet, cur_time) 
 
     
     def join(self):
